5d1d756f30c45b408f134a2c4cf7b1b54a696974
EuanPyle <euanpyle@gmail.com>
2022-05-06 14:33:16 +0100

    Complete code, untested

---
+++ lil_aretomo/lil_aretomo.py
@@ -0,0 +1,56 @@
+from os import PathLike
+from pathlib import Path
+from typing import Dict, Any, Tuple, List
+
+import subprocess
+
+from .utils import (
+    prepare_imod_directory
+    run_batchrun
+    find_binning_factor
+)
+
+
+def run_aretomo_standard_alignment(
+        tilt_series_file: Path,
+        tilt_angles: List[float],
+        pixel_size: float,
+        output_directory: Path,
+	exe: Path,
+        target_pixel_size: Optional[float] = typer.Argument(10)
+	nominal_rotation_angle: Optional[float] = typer.Argument(None)
+):
+    """Run aretomo alignment on a single tilt-series
+    
+    Parameters
+    ----------
+    
+    tilt_series_file: file containing tilt-series images
+    tilt_angles: nominal stage tilt-angles from the microscope.
+    pixel_size: pixel size of the tilt-series in angstroms-per-pixel
+    output_directory: tilt-series directory for IMOD.
+    exe: path to the AreTomo executable file
+    (optional) nominal_rotation_angle: initial estimate for the rotation angle of the tilt
+    axis. AreTomo does not need this information but it might help.
+    """
+
+    prepare_imod_directory(
+        tilt_series_file=tilt_series_file,
+        tilt_angles=tilt_angles,
+        imod_directory=output_directory
+    )
+    
+    binning=find_binning_factor(
+        pixel_size=pixel_size
+        target_pixel_size=target_pixel_size
+    )
+    
+    run_batchrun(
+        tilt_series_file=tilt_series_file,
+        imod_directory=output_directory
+	binning=binning
+    )
+
+
+
+
--- lil_aretomo/utils.py
+++ lil_aretomo/utils.py
@@ -7,19 +7,66 @@ from typing import List, Dict
 import numpy as np
 
 def prepare_imod_directory(
-        tilt_series_file: Path, tilt_angles: List[float], imod_directory: Path
+        tilt_series_file: Path, 
+        tilt_angles: List[float], 
+        imod_directory: Path
 ):
-    root_name = tilt_series_file.stem
+    ts_dir_name = tilt_series_file.stem
     imod_directory.mkdir(exist_ok=True, parents=True)
 
     tilt_series_file_for_imod = imod_directory / tilt_series_file.name
     force_symlink(tilt_series_file.absolute(), tilt_series_file_for_imod)
 
-    rawtlt_file = imod_directory / f'{root_name}.rawtlt'
+    rawtlt_file = imod_directory / f'{ts_dir_name}.rawtlt'
     np.savetxt(rawtlt_file, tilt_angles, fmt='%.2f', delimiter='')
+    
+#####
 
-def force_symlink(src: Path, link_name: Path):
-    """Force creation of a symbolic link, removing any existing file."""
-    if link_name.exists():
-        os.remove(link_name)
-    os.symlink(src, link_name)
+def run_batchrun(
+        tilt_series_file: Path, 
+        imod_directory: Path, 
+        binning: float
+):
+        
+    #Rename file .mrc if .st    
+    if tilt_series_file.suffix == '.st':
+         tilt_series_file = tilt_series_file.rename(tilt_series_file.with_suffix('.mrc'))
+    
+    output_file_name = Path(f'{tilt_series_file.stem}_aln{tilt_series_file.suffix}')
+    
+    #with tempfile.TemporaryDirectory() as temporary_directory:
+        
+	#Run Global AreTomo
+        aretomo_command = [
+            f'{str(exe)}',
+            '-InMrc', f'{tilt_series_file}',
+            '-OutMrc', f'{output_file_name}', 
+            '-OutBin', f'{binning}',
+	    '-AngFile', f'{tilt_series_file.stem}.rawtlt',
+	    '-VolZ', f'0',
+	    '-OutXF', f'1'
+        ]
+        subprocess.run(batchrun_command)
+	
+	#Rename .tlt
+        new_output_stem = output_file_name.stem[:-4]
+	new_output_name_tlt = Path(new_output_stem).with_suffix('.tlt')
+	output_file_name.rename(new_output_name_tlt)
+	
+#####
+
+def find_binning_factor(
+    pixel_size: float
+    target_pixel_size: float
+) -> int:
+    #Find closest IMOD binning to reach target pixel size
+    factors = 2 ** np.arange(7)
+    binned_pixel_sizes = factors * pixel_size
+    delta_pixel = np.abs(binned_pixel_sizes - target_pixel_size)
+    binning = factors[np.argmin(delta_pixel)]
+    return binning	
+
+
+
+
+
