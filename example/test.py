15e8ffa22e6a92621fd5bd4bc7c403fba4aab1f3
EuanPyle <euanpyle@gmail.com>
2022-05-11 14:31:41 +0100

    Commit

--- example/test.py
+++ example/test.py
@@ -10,5 +10,9 @@ run_aretomo_alignment(
     tilt_angles=np.arange(-42,51,3),
     pixel_size=1.329,
     output_directory=TEST_DATA_DIR / 'output',
-    exe='/s/ems/s/AreTomo/v1.1.0/AreTomo_1.1.0_Cuda114_03-24-2022'
+    exe='/s/ems/s/AreTomo/v1.1.0/AreTomo_1.1.0_Cuda114_03-24-2022',
+    local_align=True,
+    patch_in_x=6,
+    patch_in_y=4,
+    correct_tilt_angle_offset=True
 )
