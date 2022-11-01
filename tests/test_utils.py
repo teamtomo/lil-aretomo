54557033f6e956f07aacfb566ac5730b71762494
EuanPyle <euanpyle@gmail.com>
2022-11-01 17:14:02 +0000

    Search for correct columns, if not found is because v>=1.3 so look ag… (#41)
    * Search for correct columns, if not found is because v>=1.3 so look again in aln file
    
    * generalise parsing logic to also work for AreTomo v1.3.0 files (#42)
    
    * generalise parsing logic to also work for AreTomo v1.3.0 files, add example file and test
    
    * add EOF
    
    Co-authored-by: alisterburt <alisterburt@gmail.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,12 +3,13 @@ import pandas as pd
 from lil_aretomo.utils import read_aln
 
 
-def test_read_aln(aln_file):
+def test_read_aln(aln_file_v1_2_0, aln_file_v1_3_0):
     """Check that aln files are correctly parsed."""
-    df = read_aln(aln_file)
-    assert isinstance(df, pd.DataFrame)
-    assert df.shape == (41, 10)
-    expected_columns = [
-        'SEC', 'ROT', 'GMAG', 'TX', 'TY', 'SMEAN', 'SFIT', 'SCALE', 'BASE', 'TILT'
-    ]
+    for aln_file in (aln_file_v1_2_0, aln_file_v1_3_0):
+        df = read_aln(aln_file)
+        assert isinstance(df, pd.DataFrame)
+        assert df.shape == (41, 10)
+        expected_columns = [
+            'SEC', 'ROT', 'GMAG', 'TX', 'TY', 'SMEAN', 'SFIT', 'SCALE', 'BASE', 'TILT'
+        ]
     assert all(col in df.columns for col in expected_columns)
